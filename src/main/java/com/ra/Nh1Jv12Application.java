15481171ccfcfb20b041b5bb456f8a76feade561
your new username <smallmonster900@gmail.com>
2024-07-17 09:26:09 +0700

    iniy

---
+++ src/main/java/com/ra/Nh1Jv12Application.java
@@ -0,0 +1,13 @@
+package com.ra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nh1Jv12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nh1Jv12Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ra/Nh1Jv12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nh1Jv12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
